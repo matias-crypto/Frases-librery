adfb0c03765a14467a7c029d3c67794f306e87ba
matias-crypto <149725150+matias-crypto@users.noreply.github.com>
2024-06-30 21:06:06 -0300

    Actualizar

---
+++ futbolistas.js
@@ -0,0 +1,10 @@
+// futbolistas.js
+
+module.exports = {
+    futbolistas: [
+        { nombre: "Lionel Messi", resumen: "Nacido en 1987 en Rosario, Argentina" },
+        { nombre: "Cristiano Ronaldo", resumen: "Nacido en 1985 en Funchal, Portugal" },
+        { nombre: "Neymar", resumen: "Nacido en 1992 en Mogi das Cruzes, Brasil" },
+        
+    ]
+};
