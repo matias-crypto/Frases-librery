53e2d03877a86c1b65ec901e7105b629b95aeb65
matias-crypto <149725150+matias-crypto@users.noreply.github.com>
2025-03-16 23:07:32 -0300

    Actualizar futbolista2.js

--- futbolista2.js
+++ futbolista2.js
@@ -1,12 +1,1 @@
-// futbolista2.js
-
-const futbolista = require('./futbolistas');  
-
-function obtenerFutbolistaAleatorio() {
-    const indiceAleatorio = Math.floor(Math.random() * futbolista.length);
-    return futbolista[indiceAleatorio];
-}
-
-module.exports = {
-    obtenerFutbolistaAleatorio
-};
+//
